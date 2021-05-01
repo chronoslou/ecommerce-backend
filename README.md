faa8a225f977b4ec1eb0ba49cb438a3ca83eaf87
Lou Falla <lou3892@gmail.com>
2021-05-01 15:49:41 -0400

    added image for repo

--- README.md
+++ README.md
@@ -1,33 +1,39 @@
 # Object-Relational Mapping (ORM): E-Commerce Back End
 
-## Live Link: 
+## Live Link:
 
 ## Description
+
 This is an app for the back end for an e-commerce site. It will work with Express.js API and will use Sequelize to interact with a MySQL database.
 
 ## Usage
-* GIVEN a functional Express.js API
-* WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
-* THEN I am able to connect to a database using Sequelize
-* WHEN I enter schema and seed commands
-* THEN a development database is created and is seeded with test data
-* WHEN I enter the command to invoke the application
-* THEN my server is started and the Sequelize models are synced to the MySQL database
-* WHEN I open API GET routes in Insomnia Core for categories, products, or tags
-* THEN the data for each of these routes is displayed in a formatted JSON
-* WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
-* THEN I am able to successfully create, update, and delete data in my database
+
+- GIVEN a functional Express.js API
+- WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+- THEN I am able to connect to a database using Sequelize
+- WHEN I enter schema and seed commands
+- THEN a development database is created and is seeded with test data
+- WHEN I enter the command to invoke the application
+- THEN my server is started and the Sequelize models are synced to the MySQL database
+- WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+- THEN the data for each of these routes is displayed in a formatted JSON
+- WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+- THEN I am able to successfully create, update, and delete data in my database
 
 ## Installation
+
 The app will need:
-* [MySQL2](https://www.npmjs.com/package/mysql2)
-* [Sequelize](https://www.npmjs.com/package/sequelize) packages to connect your Express.js API to a MySQL database and the 
-* [dotenv](https://www.npmjs.com/package/dotenv) package to use environment variables to store sensitive data.
+
+- [MySQL2](https://www.npmjs.com/package/mysql2)
+- [Sequelize](https://www.npmjs.com/package/sequelize) packages to connect your Express.js API to a MySQL database and the
+- [dotenv](https://www.npmjs.com/package/dotenv) package to use environment variables to store sensitive data.
 
 Use the `schema.sql` file in the `db` folder to create your database with MySQL shell commands. Use environment variables to store sensitive data like your MySQL username, password, and database name.
 
-## Preview of App 
-![Screenshot](APPIMAGE.jpg)
+## Preview of App
+
+![Screenshot](Assets/app.gif)
 
 # Lincense
-* MIT
+
+- MIT
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,60 +1,59 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Product.findAll(
-    {
-      include: [
-        {
-          model: Category,
-          attributes: ['category_name']
-        },
-        {
-          model: Tag,
-          attributes: ['tag_name']
-        }
-      ]
-    }
-  )
-    .then(productData => res.json(productData))
-    .catch(err => {
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+  })
+    .then((productData) => res.json(productData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   Product.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
-    include: [{
-      model: Category,
-      attributes: ['category_name']
-    },
-    {
-      model: Tag,
-      attributes: ['tag_name']
-    }
-    ]
+    include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
   })
-    .then(productData => res.json(productData))
-    .catch(err => {
+    .then((productData) => res.json(productData))
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -78,7 +77,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -119,21 +118,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
   Product.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(productData => {
+    .then((productData) => {
       if (!productData) {
-        res.status(404).json({ message: 'No Product found with that ID.' });
+        res.status(404).json({ message: "No Product found with that ID." });
         return;
       }
       res.json(productData);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
